65340856131cd5c703514d0d8b4d5d9dc0c3347e
zainamroti <sayedzainjeelani@gmail.com>
2022-06-15 22:47:30 +0500

    Add user address mapping and functions

---
+++ smart-contracts/contracts/Domains.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+
+    // A mapping to store user names;
+    mapping(string => address) public domains;
+
+  constructor() {
+    console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
+  }
+
+// A register funciton that adds their names to our mapping
+
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!",msg.sender);
+    }
+
+    // This will give us the domain owners' address
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+
+}
