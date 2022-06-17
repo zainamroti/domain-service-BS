8901d0d27921e5633afcf5120dfcd303605c9d8b
zainamroti <sayedzainjeelani@gmail.com>
2022-06-17 21:28:27 +0500

    Add connect wallet button and make it work

--- nerd-name-service/pages/index.js
+++ nerd-name-service/pages/index.js
@@ -1,63 +1,66 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {TWITTER_HANDLE, TWITTER_LINK} from '../constants/index'
+import { TWITTER_HANDLE, TWITTER_LINK } from '../constants'
 import LoadingIndicator from '../components/LoadingIndicator'
+import { useState, useEffect, useRef } from 'react'
+import { providers, Contract } from "ethers";
+import Web3Modal from "web3modal";
 
 export default function Home() {
-// walletConnected keep track of whether the user's wallet is connected or not
-const [walletConnected, setWalletConnected] = useState(false);
-// loading is set to true when we are waiting for a transaction to get mined
-const [loading, setLoading] = useState(false);
-// Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
-const web3ModalRef = useRef();
+  // walletConnected keep track of whether the user's wallet is connected or not
+  const [walletConnected, setWalletConnected] = useState(false);
+  // loading is set to true when we are waiting for a transaction to get mined
+  const [loading, setLoading] = useState(false);
+  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
+  const web3ModalRef = useRef();
 
-/**
-   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
-   * signing capabilities of metamask attached
-   *
-   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
-   *
-   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
-   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
-   * request signatures from the user using Signer functions.
-   *
-   * @param {*} needSigner - True if you need the signer, default false otherwise
-   */
- const getProviderOrSigner = async (needSigner = false) => {
-  // Connect to Metamask
-  // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
-  const provider = await web3ModalRef.current.connect();
-  const web3Provider = new providers.Web3Provider(provider);
+  /**
+     * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+     * signing capabilities of metamask attached
+     *
+     * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+     *
+     * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+     * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+     * request signatures from the user using Signer functions.
+     *
+     * @param {*} needSigner - True if you need the signer, default false otherwise
+     */
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
 
-  // If user is not connected to the Rinkeby network, let them know and throw an error
-  const { chainId } = await web3Provider.getNetwork();
-  if (chainId !== 3) {
-    window.alert("Change the network to Ropsten");
-    throw new Error("Change network to Ropsten");
-  }
+    // If user is not connected to the Rinkeby network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 80001) {
+      window.alert("Change the network to Polygon Mumbai");
+      throw new Error("Change network to Polygon Mumbai");
+    }
 
-  if (needSigner) {
-    const signer = web3Provider.getSigner();
-    return signer;
-  }
-  return web3Provider;
-};
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
 
-/*
-    connectWallet: Connects the MetaMask wallet
-  */
-    const connectWallet = async () => {
-      try {
-        // Get the provider from web3Modal, which in our case is MetaMask
-        // When used for the first time, it prompts the user to connect their wallet
-        await getProviderOrSigner();
-        setWalletConnected(true);
-  
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  /*
+      connectWallet: Connects the MetaMask wallet
+    */
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
 
 
@@ -66,22 +69,30 @@ const web3ModalRef = useRef();
     if (walletConnected) {
       if (loading) {
         return <LoadingIndicator />;
-      } 
-    } else {
+      }
       return (
         <div className={styles.connectWalletContainer}>
           <img src="/assets/nerdzone.gif" alt="Nerd gif" />
           <button className={`${styles.ctaButton} ${styles.connectWalletButton}`}>
+            Connected
+          </button>
+        </div>
+      )
+    } else {
+      return (
+        <div className={styles.connectWalletContainer}>
+          <img src="/assets/nerdzone.gif" alt="Nerd gif" />
+          <button onClick={connectWallet} className={`${styles.ctaButton} ${styles.connectWalletButton}`}>
             Connect Your Wallet
           </button>
         </div>
       );
     }
-    
-    
-    
-    
-   }
+
+
+
+
+  }
 
   // useEffects are used to react to changes in state of the website
   // The array at the end of function call represents what state changes will trigger this effect
@@ -96,7 +107,7 @@ const web3ModalRef = useRef();
         providerOptions: {},
         disableInjectedProvider: false,
       });
-      connectWallet();
+      // connectWallet();
     }
   }, [walletConnected]);
 
